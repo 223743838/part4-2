227782832aa668a84d4a46f8e19be0d4b3fc5690
223743838 <162272259+223743838@users.noreply.github.com>
2024-10-02 14:30:22 +1000

    Update kubernetes-cluster.tf

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -2,11 +2,11 @@
 #
 # Creates a managed Kubernetes cluster on Azure.
 #
-resource "azurerm_kubernetes_cluster" "cluster" {
-    name                = var.app_name
+resource "azurerm_kubernetes_cluster" "sit722part05" {
+    name                = var.aks_name
     location            = var.location
-    resource_group_name = azurerm_resource_group.sit722part05.name
-    dns_prefix          = var.app_name
+    resource_group_name = var.resource_group_name
+    dns_prefix          = var.aks_name
     kubernetes_version  = var.kubernetes_version
 
     default_node_pool {
@@ -23,13 +23,11 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     }    
 }
 
-#
-# Attaches the container registry to the cluster.
-# See example here: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/container_registry#example-usage-attaching-a-container-registry-to-a-kubernetes-cluster
+network_profile {
+    network_plugin = "azure"
+  }
 
-resource "azurerm_role_assignment" "role_assignment" {
-  principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.container_registry.id
-  skip_service_principal_aad_check = true
+  depends_on = [
+    azurerm_resource_group.sit722part05
+  ]
 }
