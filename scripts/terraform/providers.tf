f78bee20b6edabf6d85317e9db2b9cc6129b93f2
223743838 <162272259+223743838@users.noreply.github.com>
2024-10-02 14:31:40 +1000

    Update providers.tf

--- scripts/terraform/providers.tf
+++ scripts/terraform/providers.tf
@@ -1,16 +1,4 @@
-# Initialises Terraform providers and sets their version numbers.
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.71.0"
-    }
-  }
-
-  required_version = ">= 1.5.6"
-}
-
 provider "azurerm" {
+  subscription_id = var.subscription_id
   features {}
 }
