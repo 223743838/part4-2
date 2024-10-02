0961390e60b9931a6321467cf4ec5bd7ce6211fb
s223743838 <s223743838@deakin.edu.au>
2024-10-02 16:10:04 +1000

    commit

--- scripts/terraform/resource-group.tf
+++ scripts/terraform/resource-group.tf
@@ -2,6 +2,6 @@
 # Creates a resource group for sit722part5 in your Azure account.
 #
 resource "azurerm_resource_group" "sit722part05" {
-  name     = var.app_name
+  name = var.app_name
   location = var.location
 }
